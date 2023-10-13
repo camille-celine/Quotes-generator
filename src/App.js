57c07cbe83ac4ea2d261ac6384dd05c03f87c277
Camille Rey <camille.celine.rey@gmail.com>
2023-10-13 15:08:03 +0200

    CTA design and logic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-import QuoteTest from "./QuoteTest";
+import QuoteList from "./QuoteList";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <QuoteTest />
+        <QuoteList />
       </div>
     );
   }
